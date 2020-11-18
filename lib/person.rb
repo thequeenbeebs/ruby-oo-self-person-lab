7d7fdf1fb3882e51057258cc542f753b1ca051f3
Blaire Baker <bakerblaire@gmail.com>
2020-11-18 16:12:37 -0600

    commit tree bd652a88060995fff3188b5ea351e33332623cc8

--- lib/person.rb
+++ lib/person.rb
@@ -26,4 +26,52 @@ class Person
         end
         @hygiene
     end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(person)
+        self.happiness += 3
+        person.happiness += 3
+        "Hi #{person.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            "blah blah sun blah rain"
+        else
+            'blah blah blah blah blah'
+        end
+    end
 end
+
+# winston = Person.new("Winston")
+# blaire = Person.new("Blaire")
+#winston.call_friend(blaire)
